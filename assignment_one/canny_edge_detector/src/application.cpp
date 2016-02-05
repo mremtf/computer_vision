d2ba2993875c0cc01ab2fe82e9f68a311ee1e161
englandmr <englandmr@mwc-120143.dhcp.missouri.edu>
2016-02-05 16:16:05 -0600

    fixed my errors

--- assignment_one/canny_edge_detector/src/application.cpp
+++ assignment_one/canny_edge_detector/src/application.cpp
@@ -140,25 +140,26 @@ int main( int argc, char** argv )
 	// Find the local maximums of the edges produced from the gradients
   for (auto r = 1; r < nms.rows - 1; r++) {
   	for (auto c = 1; c < nms.cols - 1; c++) {
-
+			// find the orientation of the gradient
 			float dir = atan2(dy.at<short>(r,c),dx.at<short>(r,c));
+			// if negative radian shift to positive region
 			if (dir < 0) {
 				dir += M_PI;
 			}
     	// check current vs east and west
-			if (dir <= 0 && dir <= M_PI/4 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r,c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r,c - 1)) {
+			if (dir <= 0 && dir < M_PI/4 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r,c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r,c - 1)) {
 				nms.at<uchar>(r,c) = magnitude.at<uchar>(r,c);
 			}
 			// check south east and north west
-			else if (dir > M_PI/4 && dir <= M_PI/2 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1, c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1 ,c - 1)) {
+			else if (dir >= M_PI/4 && dir < M_PI/2 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1, c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1 ,c - 1)) {
 				nms.at<uchar>(r,c) = magnitude.at<uchar>(r,c);
 			}
 			// check north and south
-			else if (dir > M_PI/2 && dir <= (2*M_PI)/3 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1,c) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1,c)) {
+			else if (dir >= M_PI/2 && dir < (2*M_PI)/3 && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1,c) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1,c)) {
 				nms.at<uchar>(r,c) = magnitude.at<uchar>(r,c);
 			}
 			// check north east and south west
-			else if (dir > (2*M_PI)/3 && dir <= M_PI && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1,c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1,c - 1)) {
+			else if (dir >= (2*M_PI)/3 && dir <= M_PI && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r - 1,c + 1) && magnitude.at<uchar>(r,c) > magnitude.at<uchar>(r + 1,c - 1)) {
 				nms.at<uchar>(r,c) = magnitude.at<uchar>(r,c);
 			}
 			else {
