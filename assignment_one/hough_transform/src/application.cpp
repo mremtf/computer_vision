ec86ea8e7afdaa68b23ba060520a173d3dc7acd1
mremtf <matt.r.england@gmail.com>
2016-02-11 17:48:06 -0600

    adding hough transform

---
+++ assignment_one/hough_transform/src/application.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main (int argc, char** argv) {
+	if (argc != 3) {
+		std::cout << argv[0] << " <input image> <something>" << std::endl;
+		return -1;
+	}
+
+	
+
+	return 0;
+}
